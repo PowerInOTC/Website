59ca1486b35edb3a21a37b47c549898f5e9dbcc2
Brillaugte <58943684+Brillaugte@users.noreply.github.com>
2024-04-09 21:43:41 +0300

    import correct

--- src/components/shared/TripettoForm.js
+++ src/components/shared/TripettoForm.js
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Script from "next/script";
 
 const TripettoForm = () => {
+  useEffect(() => {
+    const initForm = () => {
+      if (typeof window !== "undefined" && window.TripettoStudio) {
+        window.TripettoStudio.form({
+          runner: window.TripettoAutoscroll,
+          token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZUhBZmp1Z0owUkRhSlhVTjl6Qm9oVnlvOStodWgwZ01OdU4wc1NjYWZjND0iLCJkZWZpbml0aW9uIjoibWljZ3crai96cW44VDc5d2tTQk10NFpUSGhnZVZQbUlrbGNUMkR0WEdKND0iLCJ0eXBlIjoiY29sbGVjdCJ9.P4k3FFFkwAI-bG0RiAAXmpjX9wildEDPjYE4bxCykRQ",
+          element: "tripetto-eebn3n",
+          pausable: true,
+          persistent: true,
+        });
+      }
+    };
+
+    initForm();
+  }, []);
+
   return (
     <>
       <div id="tripetto-eebn3n"></div>
 
-      <script src="https://unpkg.com/@tripetto/runner"></script>
-      <script src="https://unpkg.com/@tripetto/runner-autoscroll"></script>
-      <script src="https://unpkg.com/@tripetto/studio"></script>
-
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            TripettoStudio.form({
-              runner: TripettoAutoscroll,
-              token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZUhBZmp1Z0owUkRhSlhVTjl6Qm9oVnlvOStodWgwZ01OdU4wc1NjYWZjND0iLCJkZWZpbml0aW9uIjoibWljZ3crai96cW44VDc5d2tTQk10NFpUSGhnZVZQbUlrbGNUMkR0WEdKND0iLCJ0eXBlIjoiY29sbGVjdCJ9.P4k3FFFkwAI-bG0RiAAXmpjX9wildEDPjYE4bxCykRQ",
-              element: "tripetto-eebn3n",
-              pausable: true,
-              persistent: true
-            });
-          `,
-        }}
+      <Script
+        src="https://unpkg.com/@tripetto/runner"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://unpkg.com/@tripetto/runner-autoscroll"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://unpkg.com/@tripetto/studio"
+        strategy="afterInteractive"
+        onLoad={initForm}
       />
     </>
   );
