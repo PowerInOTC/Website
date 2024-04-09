e3a76ef4f8907643aa74ce47bb6ffe3b6227b744
Brillaugte <58943684+Brillaugte@users.noreply.github.com>
2024-04-09 21:39:15 +0300

    import correct

--- src/app/WhitelabelDexForm/page.jsx
+++ src/app/WhitelabelDexForm/page.jsx
@@ -2,22 +2,15 @@
 import Header from "../../components/shared/Header";
 import Footer from "../../components/shared/Footer";
 import FooterUp from "../../components/shared/FooterUp";
-import { run } from "@tripetto/runner-autoscroll";
-import { Services } from "@tripetto/studio";
+import { TripettoForm } from "../../components/shared/TripettoForm";
+import React from "react";
 
 export default function WhitelabelDexForm() {
   return (
     <div>
       <Header />
       <div className="container">
-        {run(
-          Services.get({
-            token:
-              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZUhBZmp1Z0owUkRhSlhVTjl6Qm9oVnlvOStodWgwZ01OdU4wc1NjYWZjND0iLCJkZWZpbml0aW9uIjoibWljZ3crai96cW44VDc5d2tTQk10NFpUSGhnZVZQbUlrbGNUMkR0WEdKND0iLCJ0eXBlIjoiY29sbGVjdCJ9.P4k3FFFkwAI-bG0RiAAXmpjX9wildEDPjYE4bxCykRQ",
-            pausable: true,
-            persistent: true,
-          })
-        )}
+        <TripettoForm />
       </div>
       <FooterUp />
       <Footer />
---
+++ src/components/shared/TripettoForm.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const TripettoForm = () => {
+  return (
+    <>
+      <div id="tripetto-eebn3n"></div>
+
+      <script src="https://unpkg.com/@tripetto/runner"></script>
+      <script src="https://unpkg.com/@tripetto/runner-autoscroll"></script>
+      <script src="https://unpkg.com/@tripetto/studio"></script>
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            TripettoStudio.form({
+              runner: TripettoAutoscroll,
+              token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZUhBZmp1Z0owUkRhSlhVTjl6Qm9oVnlvOStodWgwZ01OdU4wc1NjYWZjND0iLCJkZWZpbml0aW9uIjoibWljZ3crai96cW44VDc5d2tTQk10NFpUSGhnZVZQbUlrbGNUMkR0WEdKND0iLCJ0eXBlIjoiY29sbGVjdCJ9.P4k3FFFkwAI-bG0RiAAXmpjX9wildEDPjYE4bxCykRQ",
+              element: "tripetto-eebn3n",
+              pausable: true,
+              persistent: true
+            });
+          `,
+        }}
+      />
+    </>
+  );
+};
+
+export default TripettoForm;
