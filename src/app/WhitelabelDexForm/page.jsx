f2ba535644720c883dfdad9b722f3e1b75add7f7
Brillaugte <58943684+Brillaugte@users.noreply.github.com>
2024-04-09 21:31:43 +0300

    import correct

--- src/app/WhitelabelDexForm/page.jsx
+++ src/app/WhitelabelDexForm/page.jsx
@@ -1,25 +1,23 @@
 "use client";
-
 import Header from "../../components/shared/Header";
 import Footer from "../../components/shared/Footer";
 import FooterUp from "../../components/shared/FooterUp";
-import { ChatRunner } from "@tripetto/runner-chat";
+import { run } from "@tripetto/runner-autoscroll";
+import { Services } from "@tripetto/studio";
 
 export default function WhitelabelDexForm() {
   return (
     <div>
       <Header />
       <div className="container">
-        <ChatRunner
-          definition={run(
-            Services.get({
-              token:
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZUhBZmp1Z0owUkRhSlhVTjl6Qm9oVnlvOStodWgwZ01OdU4wc1NjYWZjND0iLCJkZWZpbml0aW9uIjoibWljZ3crai96cW44VDc5d2tTQk10NFpUSGhnZVZQbUlrbGNUMkR0WEdKND0iLCJ0eXBlIjoiY29sbGVjdCJ9.P4k3FFFkwAI-bG0RiAAXmpjX9wildEDPjYE4bxCykRQ",
-              pausable: true,
-              persistent: true,
-            })
-          )}
-        />
+        {run(
+          Services.get({
+            token:
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZUhBZmp1Z0owUkRhSlhVTjl6Qm9oVnlvOStodWgwZ01OdU4wc1NjYWZjND0iLCJkZWZpbml0aW9uIjoibWljZ3crai96cW44VDc5d2tTQk10NFpUSGhnZVZQbUlrbGNUMkR0WEdKND0iLCJ0eXBlIjoiY29sbGVjdCJ9.P4k3FFFkwAI-bG0RiAAXmpjX9wildEDPjYE4bxCykRQ",
+            pausable: true,
+            persistent: true,
+          })
+        )}
       </div>
       <FooterUp />
       <Footer />
